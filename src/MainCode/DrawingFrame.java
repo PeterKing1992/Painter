ae4b57725af7dc86437c213edb25ee97c168e44e
PeterKing1992 <71399908+PeterKing1992@users.noreply.github.com>
2022-01-02 14:05:03 -0500

    Fixed line disconnecting bug

--- src/MainCode/DrawingFrame.java
+++ src/MainCode/DrawingFrame.java
@@ -1,15 +1,49 @@
 package MainCode;
 
+import java.awt.BorderLayout;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JToolBar;
 
 public class DrawingFrame extends JFrame{
+	private JToolBar toolBar; 
+	private JButton test; 
+	private JComboBox<String> brushDropdown;
+	
 	public DrawingFrame() {
 		initialize(); 
+		toolBarSetup(); 
 	}
+	
 	public void initialize() {
 		setTitle("Painter"); 
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); 
 		setSize(1280, 800); 
 		setVisible(true); 
+	}
+	
+	public void toolBarSetup() {
+		toolBar = new JToolBar(); 
+		test = new JButton("Test"); 
+		String[] brushSizes= {"1x1px", "2x2px", "4x4px", "8x8px", "16x16px"}; 
+		brushDropdown = new JComboBox<String>(brushSizes); 
+		brushDropdown.setVisible(true); 
+		brushDropdown.setBounds(test.WIDTH, 0, 70, toolBar.HEIGHT);
+		
+//		brushDropdown.addItemListener(new ItemListener() {
+//			public void itemStateChanged(ItemEvent event) {
+//				
+//			}
+//		});
+		
+		toolBar.add(test); 
+		toolBar.add(brushDropdown); 
+		
+		this.add(toolBar, BorderLayout.NORTH); 
+		
 	}
 }
--- src/MainCode/DrawingPanel.java
+++ src/MainCode/DrawingPanel.java
@@ -1,5 +1,6 @@
 package MainCode;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
@@ -7,11 +8,14 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JToolBar;
 
 public class DrawingPanel extends JPanel implements MouseListener, MouseMotionListener{
 	private DrawnComponent drawnComponent; 
 	private int mouseX, mouseY; 
+	private int prevMouseX, prevMouseY; 
 	private String state; 
 	
 	public DrawingPanel() {
@@ -26,20 +30,29 @@ public class DrawingPanel extends JPanel implements MouseListener, MouseMotionLi
 		addMouseListener(this); 
 		addMouseMotionListener(this); 
 		
-		mouseX = 0; 
-		mouseY = 0; 
+		mouseX = -1; 
+		mouseY = -1; 
+		prevMouseX = -1; 
+		prevMouseY = -1; 
+		
 		state = "idle"; 
 	}
 	
+	
+	
 	public void paint(Graphics g) {
-//		super.paintComponent(g); 
-		g.setColor(Color.black);
-		g.fillRect(mouseX, mouseY, 1, 1);
+		if(prevMouseX != -1) {
+			g.setColor(Color.black);
+			g.drawLine(prevMouseX, prevMouseY, mouseX, mouseY); 
+		}
 	}
 
 	@Override
 	public void mouseDragged(MouseEvent e) {
-		// TODO Auto-generated method stub
+		if(mouseX != -1) {
+			prevMouseX = this.mouseX; 
+			prevMouseY = this.mouseY; 
+		}
 		this.mouseX = e.getX(); 
 		this.mouseY = e.getY(); 
 //		this.state = "draw"; 
@@ -65,8 +78,11 @@ public class DrawingPanel extends JPanel implements MouseListener, MouseMotionLi
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
+		prevMouseX = -1; 
+		prevMouseY = -1; 
+		this.mouseX = -1; 
+		this.mouseY = -1; 
+		System.out.println("Mouse Reset");
 	}
 
 	@Override
